dc221bcddc7065bcf0c245f376e00a2bc7e0b2c7
puihayyip <puihay.yip@gmail.com>
2023-10-11 23:06:03 +0800

    Adding ex git folders

---
+++ Albums/src/main/java/com/team42/NHPS/api/Albums/AlbumsApplication.java
@@ -0,0 +1,16 @@
+package com.team42.NHPS.api.Albums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AlbumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumsApplication.class, args);
+	}
+
+}
+
