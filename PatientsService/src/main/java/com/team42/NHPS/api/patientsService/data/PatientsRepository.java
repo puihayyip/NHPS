3f6dfb48392b901c6d33beee91b5c781a263d776
puihayyip <puihay.yip@gmail.com>
2023-10-13 07:06:35 +0800

    add patients config files

--- PatientsService/src/main/java/com/team42/NHPS/api/patientsService/data/PatientsRepository.java
+++ PatientsService/src/main/java/com/team42/NHPS/api/patientsService/data/PatientsRepository.java
@@ -1,11 +1,11 @@
 package com.team42.NHPS.api.patientsService.data;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface PatientsRepository extends JpaRepository<PatientsEntity, String> {
+public interface PatientsRepository extends CrudRepository<PatientsEntity, String> {
     Optional<PatientsEntity> findByNric(String nric);
 }
