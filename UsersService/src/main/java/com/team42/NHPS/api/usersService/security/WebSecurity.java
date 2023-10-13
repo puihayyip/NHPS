11251a720d4cd215fe94366cae7b5c05dfcc2569
Yip Pui Hay <puihay.yip@ncs.com.sg>
2023-10-13 16:07:20 +0800

    Reconfigured properties for collaboration

--- UsersService/src/main/java/com/team42/NHPS/api/usersService/security/WebSecurity.java
+++ UsersService/src/main/java/com/team42/NHPS/api/usersService/security/WebSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -50,6 +51,7 @@ public class WebSecurity {
                 .authorizeHttpRequests(authorize ->
                         authorize
                                 .requestMatchers(AUTH_WHITELIST).permitAll()
+                                .requestMatchers(HttpMethod.POST,"/users/login").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .addFilter(new AuthorizationFilter(authenticationManager, env))
